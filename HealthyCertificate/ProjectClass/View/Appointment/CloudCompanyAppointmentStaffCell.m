26ec35ff54a44cbfb6b2689a94fdd06eb6df3872
HighmoreJx <1059126129@qq.com>
2016-02-26 16:35:04 +0800

    服务点点击详情
    服务点点击详情

--- HealthyCertificate/ProjectClass/View/Appointment/CloudCompanyAppointmentStaffCell.m
+++ HealthyCertificate/ProjectClass/View/Appointment/CloudCompanyAppointmentStaffCell.m
@@ -11,13 +11,23 @@
 #import <Masonry.h>
 
 #import "UIFont+Custom.h"
+#import "UILabel+FontColor.h"
+#import "UIColor+Expanded.h"
 #import "Constants.h"
 
 #define Cell_Font 23
 
+
+@interface CloudCompanyAppointmentStaffCell()
+{
+    UILabel         *_countLabel;
+}
+
+@end
+
 @implementation CloudCompanyAppointmentStaffCell
 
-
+#pragma mark - Life Circle
 -(id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
     if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier])
@@ -27,10 +37,10 @@
         titleLabel.text = @"添加员工列表";
         [self addSubview:titleLabel];
         
-        UILabel* countLabel = [[UILabel alloc] init];
-        countLabel.font = [UIFont fontWithType:UIFontOpenSansRegular size:FIT_FONTSIZE(Cell_Font)];
-        countLabel.text = [NSString stringWithFormat:@"已添加%ld", _staffCount];
-        [self addSubview:countLabel];
+        _countLabel = [[UILabel alloc] init];
+        _countLabel.font = [UIFont fontWithType:UIFontOpenSansRegular size:FIT_FONTSIZE(Cell_Font)];
+        _countLabel.text = [NSString stringWithFormat:@"已添加%ld", _staffCount];
+        [self addSubview:_countLabel];
         
         UIImageView* arrowImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"arrow"]];
         [self addSubview:arrowImageView];
@@ -43,7 +53,7 @@
         [titleLabel setContentCompressionResistancePriority:751 forAxis:UILayoutConstraintAxisHorizontal];
         
 
-        [countLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        [_countLabel mas_makeConstraints:^(MASConstraintMaker *make) {
             make.centerY.mas_equalTo(self);
             make.left.greaterThanOrEqualTo(titleLabel.mas_right);
         }];
@@ -52,11 +62,21 @@
         [arrowImageView mas_makeConstraints:^(MASConstraintMaker *make) {
             make.right.mas_equalTo(self).with.offset(-PXFIT_WIDTH(24));
             make.centerY.mas_equalTo(self);
-            make.left.greaterThanOrEqualTo(countLabel.mas_right).with.offset(PXFIT_WIDTH(24));
+            make.left.greaterThanOrEqualTo(_countLabel.mas_right).with.offset(PXFIT_WIDTH(24));
         }];
         [arrowImageView setContentCompressionResistancePriority:751 forAxis:UILayoutConstraintAxisHorizontal];
     }
     return self;
+}
+
+#pragma mark - Setter & Getter
+-(void)setStaffCount:(NSInteger)staffCount{
+    _staffCount = staffCount;
+
+    [_countLabel setText:@"已添加"
+                    Font:[UIFont fontWithType:UIFontOpenSansRegular size:FIT_FONTSIZE(Cell_Font)]
+                   count:_staffCount
+                endColor:MO_RGBCOLOR(0, 168, 234)];
 }
 
 @end
