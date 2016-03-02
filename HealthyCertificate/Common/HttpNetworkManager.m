d2b8b455a4b91051d52240ae77aaa3adffb6c30a
rztime <rztime@vip.qq.com>
2016-03-02 15:37:18 +0800

    ..
    ..

--- HealthyCertificate/Common/HttpNetworkManager.m
+++ HealthyCertificate/Common/HttpNetworkManager.m
@@ -151,7 +151,7 @@ static NSString * const AFHTTPRequestOperationBaseURLString = @"http://zkwebserv
     NSInteger type = GetUserType;
     // 个人
     if (type == 1) {
-        NSString *url = [NSString stringWithFormat:@"customerTest/findMyCheckList?customId=%@&cCheckType=1&linkPhone=%@", gPersonInfo.mCustCode,gPersonInfo.StrTel];
+        NSString *url = [NSString stringWithFormat:@"customerTest/findMyCheckList?customId=%@&cCheckType=1&linkPhone=%@", gPersonInfo.CustId, gPersonInfo.StrTel];
         [self.sharedClient GET:url parameters:nil success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
             NSMutableArray *brArray = [[NSMutableArray alloc]init];
             for (NSDictionary *dict in responseObject) {
