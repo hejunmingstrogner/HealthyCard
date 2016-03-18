1292b7a27eb876bc04c4903543a666a478a707ae
rztime <rztime@vip.qq.com>
2016-03-18 16:13:00 +0800

    添加用户协议类
    添加用户协议类

---
+++ HealthyCertificate/ProjectClass/Controller/LeftMenuControllers/ConsumerAgreement.h
@@ -0,0 +1,13 @@
+//
+//  ConsumerAgreement.h
+//  HealthyCertificate
+//
+//  Created by 乄若醉灬 on 16/3/18.
+//  Copyright © 2016年 JIANGXU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConsumerAgreement : UIViewController
+
+@end
