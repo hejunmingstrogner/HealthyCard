2f59b07494d58b958f8fc3351ace585647972aa0
rztime <rztime@vip.qq.com>
2016-03-19 17:03:23 +0800

    修复小bug，以及用户协议完善
    修复小bug，以及用户协议完善

--- HealthyCertificate/ProjectClass/Controller/LeftMenuControllers/ConsumerAgreement.m
+++ HealthyCertificate/ProjectClass/Controller/LeftMenuControllers/ConsumerAgreement.m
@@ -9,9 +9,12 @@
 #import "ConsumerAgreement.h"
 #import "UIButton+Easy.h"
 #import "UIButton+HitTest.h"
-@interface ConsumerAgreement()
+#import "RzAlertView.h"
+@interface ConsumerAgreement()<UIWebViewDelegate>
 {
     UIWebView *webView;
+    RzAlertView *waitAlertView;
+    UIButton  *reloadBtn;
 }
 @end
 
@@ -49,16 +52,50 @@
 {
     webView = [[UIWebView alloc]initWithFrame:self.view.frame];
     webView.dataDetectorTypes = UIDataDetectorTypeAll;
+    webView.delegate = self;
     [self.view addSubview:webView];
+
+    reloadBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    reloadBtn.frame = self.view.frame;
+    [self.view addSubview:reloadBtn];
+    reloadBtn.titleLabel.font = [UIFont systemFontOfSize:14];
+    [reloadBtn setTitle:@"点击重新加载" forState:UIControlStateNormal];
+    [reloadBtn addTarget:self action:@selector(loadURLView) forControlEvents:UIControlEventTouchUpInside];
+    [reloadBtn setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+    reloadBtn.hidden = YES;
+
+    if (!waitAlertView) {
+        waitAlertView = [[RzAlertView alloc]initWithSuperView:self.view Title:@"加载中..."];
+    }
+    [waitAlertView show];
 }
 
 - (void)loadURLView
 {
-    NSString *str = @"https://www.baidu.com";
+    NSString *str = @"http://webserver.zeekstar.com/webserver/appProtocol.html";
     NSURL *url = [NSURL URLWithString:str];
 
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
 
     [webView loadRequest:request];
 }
+
+#pragma mark -web delegate
+- (void)webViewDidStartLoad:(UIWebView *)webView
+{
+    reloadBtn.hidden = YES;
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    [waitAlertView close];
+    reloadBtn.hidden = YES;
+}
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+    [waitAlertView close];
+    reloadBtn.hidden = NO;
+    [RzAlertView showAlertLabelWithTarget:self.view Message:@"加载失败，请检查网络后重试" removeDelay:3];
+}
+
 @end
