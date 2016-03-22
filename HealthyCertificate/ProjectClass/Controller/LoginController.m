cd2b0e725a3461429bf32258eed69b3d59bba937
HighmoreJx <1059126129@qq.com>
2016-03-22 13:58:05 +0800

    部分bug修复及ui
    部分bug修复及ui

--- HealthyCertificate/ProjectClass/Controller/LoginController.m
+++ HealthyCertificate/ProjectClass/Controller/LoginController.m
@@ -230,9 +230,6 @@ typedef NS_ENUM(NSInteger, LOGINTEXTFIELD)
 -(void)loginBtnCliked:(id)sender
 {
     _loginButton.backgroundColor = [UIColor colorWithRGBHex:HC_Base_Blue];
-    [[HMNetworkEngine getInstance] askLoginInfo:_phoneNumTextField.text];
-    _isVertified = YES;
-    return;
     //判断验证码是否正确
     [[HttpNetworkManager getInstance] vertifyPhoneNumber:_phoneNumTextField.text
                                              VertifyCode:_vertifyTextField.text
