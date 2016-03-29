f3ebffcf945b448b80c384d5f20e7addc2addfbb
HighmoreJx <1059126129@qq.com>
2016-03-29 20:04:39 +0800

    身份证扫描替换
    身份证扫描替换

--- HealthyCertificate/Common/HttpNetworkManager.m
+++ HealthyCertificate/Common/HttpNetworkManager.m
@@ -36,9 +36,10 @@
 //static NSString * const AFHTTPRequestOperationBaseURLString = @"http://222.18.159.34:8080/zkwebservice/webservice/";
 //static NSString * const AFHTTPRequestOperationBaseURLString = @"http://zkwebservice.witaction.com:808/zkwebservice/webservice/";
 //static NSString * const AFHTTPRequestOperationBaseURLString = @"http://lyx.witaction.com/zkwebservice";
-
+//http://zkwebserver.witaction.com:8080/
 // 开发环境
-static NSString * const AFHTTPRequestOperationBaseURLString = @"http://zkwebserver.witaction.com:8080/webserver/webservice";
+//static NSString * const AFHTTPRequestOperationBaseURLString = @"http://zkwebserver.witaction.com:8080";
+static NSString * const AFHTTPRequestOperationBaseURLString = @"http://zkwebserver.witaction.com:8080/webserver/webservice/";
 
 // 运营环境
 //static NSString * const AFHTTPRequestOperationBaseURLString = @"http://webserver.zeekstar.com/webserver/webservice/";
