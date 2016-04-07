a8e5641e73508c5b21284ed6227717fcc0b2f6a3
rztime <rztime@vip.qq.com>
2016-04-07 15:23:46 +0800

    修改个人历史记录界面
    修改个人历史记录界面

--- HealthyCertificate/ProjectClass/Controller/LeftMenuControllers/PersonalHistoryController.m
+++ HealthyCertificate/ProjectClass/Controller/LeftMenuControllers/PersonalHistoryController.m
@@ -165,6 +165,9 @@
 }
 - (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
 {
+    if (section == _historyArray.count - 1) {
+        return 10;
+    }
     return 0.1;
 }
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -256,7 +259,7 @@
 // 报告按钮点击
 - (void)reportBtnClicked:(CustomButton *)sender
 {
-    CustomerTest* selectCustomerTest = (CustomerTest *)_historyArray[sender.tag];
+    CustomerTest* selectCustomerTest = ((HistoryModel *)_historyArray[sender.tag]).customer;
     [HMNetworkEngine getInstance].delegate = self;
     [[HMNetworkEngine getInstance] getReportQueryUrl:selectCustomerTest.checkCode];
 }
