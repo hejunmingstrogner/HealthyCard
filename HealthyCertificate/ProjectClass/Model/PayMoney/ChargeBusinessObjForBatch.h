e7fbf239561648b2fa39e23c31121549c9001b0a
HighmoreJx <1059126129@qq.com>
2016-04-14 21:33:54 +0800

    员工体检批量支付
    员工体检批量支付

---
+++ HealthyCertificate/ProjectClass/Model/PayMoney/ChargeBusinessObjForBatch.h
@@ -0,0 +1,22 @@
+//
+//  ChargeBusinessObjForBatch.h
+//  HealthyCertificate
+//
+//  Created by JIANGXU on 16/4/14.
+//  Copyright © 2016年 JIANGXU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "ChargeBusinessObj.h"
+
+
+/**
+ *  批量支付
+ */
+@interface ChargeBusinessObjForBatch : ChargeBusinessObj
+
+//支付明细 存放ETDetail
+@property (nonatomic, copy) NSArray* details;
+
+@end
